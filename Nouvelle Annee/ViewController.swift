80bec3cd0738b0277d7e5cc24bfd7e934bb0489d
H Goktekin <haticegoktekinn@gmail.com>
2019-08-03 13:49:52 +0300

    second commit

--- Nouvelle Annee/ViewController.swift
+++ Nouvelle Annee/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("bonne aniversaire")
         print("c'est un bon histoire.")
+        print("je me suis abandonne")
         // Do any additional setup after loading the view.
     }
 
