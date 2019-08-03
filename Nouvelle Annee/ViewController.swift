711dc9bcf20639a9751813d5c4d38ff1834716fe
H Goktekin <haticegoktekinn@gmail.com>
2019-08-03 13:48:55 +0300

    first commit of all

---
+++ Nouvelle Annee/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Nouvelle Annee
+//
+//  Created by Hatice Göktekin on 3.08.2019.
+//  Copyright © 2019 Hatice Göktekin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("bonne aniversaire")
+        print("c'est un bon histoire.")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
